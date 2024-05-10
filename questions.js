5073bd6286f9c72f5200e27dca6713908187d2aa
NicatSadiqov <nicatsadiiqov@gmail.com>
2024-05-10 09:07:32 +0400

    uuhjjhjk

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "JavaScript nədir?",
+        answer: "C. Bir proqramlaşdırma dilidir.",
+        options: [
+            "A. Bir tarayıcıdır.",
+            "B. Bir məlumat bazası.",
+            "C. Bir proqramlaşdırma dilidir.",
+            "D. Bir verilən qrafikası."
+        ]
+    },
+    {
+        numb: 2,
+        question: "JavaScript-də 'DOM' nədir?",
+        answer: "B. Səhifə obyekt modeli.",
+        options: [
+            "A. Səhifənin daxili işarələri.",
+            "B. Səhifə obyekt modeli.",
+            "C. Səhifənin dizaynı və məzmunu.",
+            "D. JavaScript obyektlərinin modellənməsi."
+        ]
+    },
+    {
+        numb: 3,
+        question: "JavaScript-də 'let' və 'var' arasındakı fərq nədir?",
+        answer: "A. 'let' lokal, 'var' isə global təyinat edilir.",
+        options: [
+            "A. 'let' lokal, 'var' isə global təyinat edilir.",
+            "B. 'let' ilə yalnız rəqəmlər, 'var' ilə simvollar.",
+            "C. 'let' yalnız dəyişdirilə bilən dəyişkənləri təyin edir, 'var' sabit dəyişkənləri.",
+            "D. 'let' və 'var' eyni funksionallığa malikdir."
+        ]
+    },
+    {
+        numb: 4,
+        question: "JavaScript-də 'NaN' nədir?",
+        answer: "C. Not-a-Number, rəqəm ifadə edə bilməyən dəyər.",
+        options: [
+            "A. Yalnız mətn dəyər.",
+            "B. Nəzəri və ya təməl funksionallıq.",
+            "C. Not-a-Number, rəqəm ifadə edə bilməyən dəyər.",
+            "D. Təhlükəsizlik problemi."
+        ]
+    },
+    {
+        numb: 5,
+        question: "JavaScript-də 'typeof' operatoru nə üçündür?",
+        answer: "D. Dəyişkən dəyərinin tipini almaq üçündür.",
+        options: [
+            "A. Funksionallığın dəyərini almaq.",
+            "B. Daxil olunan məlumatın tipini təyin etmək.",
+            "C. Rəqəm dəyərinin məzmununu dəyişdirmək.",
+            "D. Dəyişkən dəyərinin tipini almaq üçündür."
+        ]
+    }
+];
